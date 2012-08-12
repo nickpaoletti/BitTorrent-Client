4fdf2ed95970e0ab52406acb9970fb65c699a36d
Danielgit config --global user.name Danielgit config --global user.name Daniel <danielhmcs@gmail.com>
2012-08-12 08:46:27 -0400

    gui added.

---
+++ src/btclient/gui/DownloadBar.java
@@ -0,0 +1,43 @@
+package btclient.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.math.BigInteger;
+import javax.swing.JComponent;
+
+public class DownloadBar extends JComponent{
+	private static final long serialVersionUID = 1L;
+	private boolean[] chunkArray = null;
+	private BigInteger numBytes = null;
+	private static final int MAX_LENGTH = 400;
+	private static final int HEIGHT = 20;
+	private double chunkWidth = -1;
+	public DownloadBar(boolean[] chunkArray){
+		setMinimumSize(new Dimension(MAX_LENGTH, HEIGHT));
+		setPreferredSize(new Dimension(MAX_LENGTH, HEIGHT));
+		setMaximumSize(new Dimension(MAX_LENGTH, HEIGHT));
+		this.chunkArray = chunkArray;
+		calculateChunkWidth();	
+	}
+	private void calculateChunkWidth(){
+		chunkWidth = (double)MAX_LENGTH/ (double)chunkArray.length;
+	}
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g.create();
+		double start = 0.0;
+		for(boolean b: chunkArray){
+			if(b){
+				g2d.setPaint(Color.blue);
+			}else{
+				g2d.setPaint(Color.RED);
+			}
+			Rectangle2D r2d = new Rectangle2D.Double(start,0,chunkWidth,HEIGHT);
+			g2d.fill(r2d);
+			start += chunkWidth;
+		}
+	}
+}
---
+++ src/btclient/gui/TorrentView.java
@@ -0,0 +1,139 @@
+package btclient.gui;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+public class TorrentView extends JFrame{
+	private static final long serialVersionUID = 2160729035270663944L;
+	private static final String aboutText = "About Text goes here!";
+	private TorrentViewContent content;
+	private boolean[] chunky;
+	private Thread t;
+	private JMenuBar menu;
+	private JMenuItem open;
+	private JMenuItem about;
+	private JMenu file;
+	private JMenu help;
+	private JMenuItem quit;
+	public TorrentView(boolean[] chunks){
+		super("Bitorrent Client");
+		file = new JMenu("File");
+		help = new JMenu("Help");
+		open = new JMenuItem("Open");
+		about = new JMenuItem("About");
+		quit = new JMenuItem("Exit");
+		menu = new JMenuBar();
+		file.add(open);
+		file.add(quit);
+		help.add(about);
+		menu.add(file);
+		menu.add(help);
+		
+		open.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// TODO Auto-generated place file choose dialog here.
+				
+				JOptionPane.showInputDialog("Enter a path to download to:");
+			}
+		});
+		about.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//JOptionPane.showMessageDialog(TorrentView.this,aboutText);
+				JOptionPane.showMessageDialog(TorrentView.this, aboutText, "Title", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("icon.jpg"));
+				
+			}
+			
+		});
+		chunky = chunks;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600,600);
+		setLocationRelativeTo(null);
+		content = new TorrentViewContent(chunks);
+		setLayout(new BorderLayout());
+		add(content,BorderLayout.CENTER);
+		add(menu,BorderLayout.NORTH);
+		setVisible(true);
+	}
+	public void setPercentage(int num){
+		content.setProgressbarPercentage(num);
+	}
+	public void updateGraphics(){
+		content.updateBar();
+	}
+	public void updatePeers(int num){
+		content.updatePeers(num);
+	}
+	public void setFilename(String s){
+		content.setFilename(s);
+	}
+	public void setFileSize(String s){
+		content.setFileSize(s);
+	}
+	public void setUploadSpeed(String s){
+		content.setUploadSpeed(s);
+	}
+	public void setDownloadSpeed(String s){
+		content.setDownloadSpeed(s);
+	}
+	public void setSeedsPeers(String s){
+		content.setSeedsPeers(s);
+	}
+	public void setFileChunkRatio(String s){
+		content.setFileChunkRatio(s);
+	}
+	public void setProgressbarPercentage(int num){
+		content.setProgressbarPercentage(num);
+	}
+	public void createTestThread(){
+		t = new Thread(new Runnable(){
+			public void run(){
+				int trueCount = 0;
+				content.setFilename("Nesticle.exe");
+				content.setFileSize("Over 9000");
+				Random rng = new Random();
+				while(trueCount < chunky.length){
+					int index = rng.nextInt(chunky.length);
+					if(chunky[index]){
+						// do nothing
+					}else{
+						chunky[index] = true;
+						updateGraphics();
+						trueCount++;
+						content.setUploadSpeed(rng.nextInt(900) + "Kb/s");
+						content.setDownloadSpeed(rng.nextInt(900) + "Kb/s");
+						content.setFileChunkRatio(trueCount + "/" + chunky.length);
+						content.setProgressbarPercentage((int)(1000 * (double)trueCount/(double)chunky.length));
+						content.setSeedsPeers(rng.nextInt(100) + "/" + rng.nextInt(300));
+					}
+					try {
+						Thread.sleep(800);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		});
+		
+	}
+	public void startTestThread(){
+		t.start();
+	}
+	public static void main(String[] args){
+		final boolean[] chunky = new boolean[100];
+		TorrentView tv = new TorrentView(chunky);
+		tv.createTestThread();
+		tv.startTestThread();	
+	}
+}
